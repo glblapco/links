0c25b42537a1ba290b98bfedd5a6805493d4fdb2
Biel <bielp07@gmail.com>
2022-01-02 17:26:34 -0300

    Add HandleRequest().

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,1 +1,27 @@
 package handlers
+
+import (
+	"log"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"proj/links/models"
+	"proj/links/repository"
+)
+
+func HandleRequest(rw http.ResponseWriter, req *http.Request) {
+	log.Println("Request received: ". req.Method)
+
+	switch req.Method {
+		case http.MethodGet:
+			list(rw, req)
+			break
+		case http.MethodPost:
+			add(rw, req)
+			break
+		default:
+			rw.WriteHeader(405)
+			rw.Write([]byte("Method not allowed."))
+			break
+	}
+}
