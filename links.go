8bb321344e88c0ec72fe00aa3742a2dcb2b23162
Biel <bielp07@gmail.com>
2022-01-02 17:49:09 -0300

    Add HandleFunc.

--- links.go
+++ links.go
@@ -3,9 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+	"proj/links/handlers"
 )
 
 func main() {
+	http.HandleFunc("/", handlers.HandleRequest)
+
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		log.Println(err)
